f7eab97c6fd51dfaa8f3fe46c0c1bf58659f97e5
neiro2222 <rzhechkovskii.egor@lit1533.ru>
2025-06-09 23:19:39 +0300

    6

---
+++ app/src/main/java/com/example/luminarytrading/Activity/DetailCryptoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.luminarytrading.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.luminarytrading.R
+
+class DetailCryptoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_activity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
