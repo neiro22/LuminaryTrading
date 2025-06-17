ab52b39a732f2922401f131a3f280aca50239cee
neiro2222 <rzhechkovskii.egor@lit1533.ru>
2025-06-17 11:36:19 +0300

    try2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
+    id("kotlin-kapt")
 }
 
 android {
@@ -38,12 +39,22 @@ android {
         dataBinding = true
         viewBinding = true
     }
+
+    configurations.all {
+        resolutionStrategy {
+            // Force a specific version of annotations to resolve conflicts
+            force("org.jetbrains:annotations:23.0.0")
+        }
+    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx) {
+        exclude(group = "com.intellij", module = "annotations")
+    }
+    implementation(libs.androidx.appcompat) {
+        exclude(group = "com.intellij", module = "annotations")
+    }
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
@@ -51,7 +62,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("com.github.majorkik:SparkLineLayout:1.0.1")
+    implementation("com.github.majorkik:SparkLineLayout:1.0.1") {
+        exclude(group = "com.intellij", module = "annotations")
+    }
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.google.android.material:material:1.12.0") // последняя на июнь 2025
 
@@ -72,4 +85,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
+
+    // Use a single version of JetBrains annotations
+    implementation("org.jetbrains:annotations:23.0.0")
 }
